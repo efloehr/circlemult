eb087de8f0ad602527103de1cf18bfa2f9486857
Eric Floehr <eric@intellovations.com>
2017-01-08 17:06:20 -0500

    Initial attempt to draw lines

--- circlemult.py
+++ circlemult.py
@@ -1,13 +1,39 @@
 import sys
+import math
 from PyQt5.QtCore import QRectF
 from PyQt5.QtGui import QPainter
 from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
 
+radians = 2.0 * math.pi
 
-# This class draws the circle
+def pointToCoords(point, modulus, radius):
+    theta = radians * (point / modulus)
+    x = radius * math.cos(theta)
+    y = radius * math.sin(theta)
+    return (x,y)
+    
+def getLineSegments(multiplier, modulus, radius):
+    segments = []
+
+    for start in range(modulus):
+        end = (start * multiplier) % modulus
+        segments.append( (pointToCoords(start, modulus, radius), pointToCoords(end, modulus, radius)) )
+
+    return segments
+
+
+# This class draws the circle and lines
 class CircleWidget(QWidget):
     def __init__(self, parent=None):
         super(CircleWidget, self).__init__(parent)
+        self.multiplier = 0
+        self.modulus = 0
+        
+    def setMultiplier(self, multiplier):
+        self.multiplier = multiplier
+        
+    def setModulus(self, modulus):
+        self.modulus = modulus
         
     def paintEvent(self, event):
         painter = QPainter(self)
@@ -15,10 +41,16 @@ class CircleWidget(QWidget):
         painter.translate(self.width() / 2, self.height() / 2)
 
         diameter = min(self.width(), self.height())
+        radius = diameter / 2.0
         
-        painter.drawEllipse(QRectF(-diameter / 2.0,
-                                   -diameter / 2.0, diameter, diameter))
+        painter.drawEllipse(QRectF(-radius, -radius, diameter, diameter))
 
+        segments = getLineSegments(10, 200, radius)
+        for segment in segments:
+            start = segment[0]
+            end = segment[1]
+            painter.drawLine(start[0], start[1], end[0], end[1])
+            
 
 # This class sets up the main window
 class MainWindow(QWidget):
