e51b49a487b722a25a56c0da035027e76c5eb9f9
Eric Floehr <eric@intellovations.com>
2017-01-08 16:29:33 -0500

    Add circle drawing

--- circlemult.py
+++ circlemult.py
@@ -1,5 +1,24 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import QRectF
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout
+
+
+# This class draws the circle
+class CircleWidget(QWidget):
+    def __init__(self, parent=None):
+        super(CircleWidget, self).__init__(parent)
+        
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.Antialiasing, True)
+        painter.translate(self.width() / 2, self.height() / 2)
+
+        diameter = min(self.width(), self.height())
+        
+        painter.drawEllipse(QRectF(-diameter / 2.0,
+                                   -diameter / 2.0, diameter, diameter))
+
 
 # This class sets up the main window
 class MainWindow(QWidget):
@@ -9,6 +28,14 @@ class MainWindow(QWidget):
         self.initUI()
         
     def initUI(self):
+        # Set up the layout
+        layout = QHBoxLayout()
+        self.setLayout(layout)
+        
+        # Add the circle widget
+        circle = CircleWidget()
+        layout.addWidget(circle)
+        
         self.setGeometry(200, 200, 800, 600)
         self.setWindowTitle('Multiplication Circle')
         self.show()
