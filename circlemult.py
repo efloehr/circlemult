b81d9a3ada57d40f8036bb06aec652a4f9ef19f3
Eric Floehr <eric@intellovations.com>
2017-01-08 15:52:58 -0500

    Add comments

--- circlemult.py
+++ circlemult.py
@@ -3,13 +3,18 @@ from PyQt5.QtWidgets import QApplication, QWidget
 
 
 if __name__ == '__main__':
-    
+    # A single Application is required.
     app = QApplication(sys.argv)
 
+    # A QWidget without a parent is a window.
     w = QWidget()
     w.resize(800, 600)
     w.move(200, 200)
     w.setWindowTitle('Multiplication Circle')
+    
+    # Show the window.
     w.show()
     
+    # Enter the event loop, waiting for events.
+    # When the application is closed, the program will exit.
     sys.exit(app.exec_())
