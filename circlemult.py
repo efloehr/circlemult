c9462cc7a0ac6975823dc47af4e6339783c16f0b
Eric Floehr <eric@intellovations.com>
2017-01-08 16:01:05 -0500

    Change to class for main window

--- circlemult.py
+++ circlemult.py
@@ -1,19 +1,25 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
 
-
+# This class sets up the main window
+class MainWindow(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        
+    def initUI(self):
+        self.setGeometry(200, 200, 800, 600)
+        self.setWindowTitle('Multiplication Circle')
+        self.show()
+        
+        
 if __name__ == '__main__':
     # A single Application is required.
     app = QApplication(sys.argv)
 
-    # A QWidget without a parent is a window.
-    w = QWidget()
-    w.resize(800, 600)
-    w.move(200, 200)
-    w.setWindowTitle('Multiplication Circle')
-    
-    # Show the window.
-    w.show()
+    # Start the main window
+    window = MainWindow()
     
     # Enter the event loop, waiting for events.
     # When the application is closed, the program will exit.
